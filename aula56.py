5ae35b6f2257e7a1b10b418a99177c6223061c16
Taylan77 <taylanbarbosa73@gmail.com>
2024-06-13 15:03:58 -0300

    Create aula56.py

---
+++ aula56.py
@@ -0,0 +1,8 @@
+#          E  X  E  R  C  I  C  I  O
+"""
+Faça uma lista de compras com listas
+O usuario deve ter a possibilidade de inserir,
+apagar e listar valores da sua lista
+Nao permita que o programa quebre com erros
+de indices inexistentes na lista.
+"""
